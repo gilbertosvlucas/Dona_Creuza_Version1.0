011e7b60da4ca7aab79259f9edc8210b55f8001d
Gilberto <gilberto@setefer.sao>
2025-04-23 15:21:24 -0300

    first commit

--- config.php
+++ config.php
@@ -7,13 +7,13 @@ $dbName = 'formulariodonacreuza'
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-if ($conexao->connect_errno)
-{
-    echo "Erro";
-}
-else
-{
-    echo "conexão efetuada com sucesso";
-}
+//if ($conexao->connect_errno)
+//{
+//    echo "Erro";
+//}
+//else
+//{
+//    echo "conexão efetuada com sucesso";
+//}
 
 ?>
