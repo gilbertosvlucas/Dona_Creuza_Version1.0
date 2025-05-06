191bcb2d4310b98dc5f22b73a67f9cee75e3f8ed
Gilberto Lucas <gilbertosvlucas@gmail.com>
2025-05-06 14:54:12 -0300

    Update config.php

--- config.php
+++ config.php
@@ -7,12 +7,12 @@ $dbName = 'formulariodonacreuza'
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-if ($conexao->connect_errno)
-{
-    echo "Erro";
-}
-else
-{
-    echo "conexão efetuada com sucesso";
-}
+//if ($conexao->connect_errno)
+//{
+//    echo "Erro";
+//}
+//else
+//{
+//    echo "conexão efetuada com sucesso";
+//}
 ?>
