ebd195950c358d6bcf63a9a4a4f77eea239ed5be
Gilberto <gilbertosvlucas@gmail.com>
2025-05-06 14:34:52 -0300

    Projeto Dona_Creuza com conexão bd Workbench

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formulariodonacreuza';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+//    echo "Erro";
+//}
+//else 
+//{
+//    echo "Conexão efetuada com sucesso";
+//}
+
+?>
